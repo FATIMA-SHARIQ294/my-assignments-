94fcc9f9c0722a0ff55b87ed75dd91eede5d8d9f
Fatima-shariq-07 <162745490+FATIMA-SHARIQ294@users.noreply.github.com>
2024-03-29 22:35:17 +0500

    Assignments 6
    Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.

---
+++ NO.6/app.js
@@ -0,0 +1,6 @@
+console.log("NAME OF POSITION HOLDERS");
+console.log(" 1.\tALIA SHAH\n 2.\tNIMRA KHAN\n 3.\tAYESHA SHARIQ\n 4.\tSIMRA AKMAL\n");
+var personName = "\n\tBABAR AZAM\n\t";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ NO.6/app.ts
@@ -0,0 +1,7 @@
+console.log("NAME OF POSITION HOLDERS");
+console.log(" 1.\tALIA SHAH\n 2.\tNIMRA KHAN\n 3.\tAYESHA SHARIQ\n 4.\tSIMRA AKMAL\n");
+var personName ="\n\tBABAR AZAM\n\t";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
+// strip means to, remove all coverings from. strip means to remove or empty.
