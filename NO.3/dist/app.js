baf6c614ab7408decc04f69ce58044cb53e2a708
Fatima-shariq-07 <162745490+FATIMA-SHARIQ294@users.noreply.github.com>
2024-03-29 13:12:31 +0500

    Assignment 3
    This is my third assignment...you might be wondering why I didn't post the first two. Well, the first two assignments only entailed downloading the application and writing "hello world".

---
+++ NO.3/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// this is my uppercase program 
+let myName = "MY NAME IS FATIMA.";
+console.log(myName);
+// this is my lowercase program
+let myClass = "i study in class 11.";
+console.log(myClass);
+// tis is my titlecase program
+let mySubject = "My main academic subject is computer.";
+console.log(mySubject);
