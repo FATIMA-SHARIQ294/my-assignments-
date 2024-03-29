9ff605fe3a8426cc4ca6d42ba515c7133480a1b7
Fatima-shariq-07 <162745490+FATIMA-SHARIQ294@users.noreply.github.com>
2024-03-29 23:02:07 +0500

    Assignment 7
    Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.

---
+++ NO.7/app.js
@@ -0,0 +1,40 @@
+// let number1 =20
+// let number1 =40
+// the above code is am error 
+console.log("Addition of numbers to get number '8'");
+var number1 = 4;
+console.log("NUMBER 1 IS");
+console.log(number1);
+var number2 = 4;
+console.log("NUMBER 2 IS");
+console.log(number2);
+console.log("RESULTS ARE:");
+console.log(number1 + number2);
+// but by using variable we can store same constants
+console.log("Subtraction of numbers to get number '8'");
+var number1 = 10;
+console.log("NUMBER 1 IS");
+console.log(number1);
+var number2 = 2;
+console.log("NUMBER 2 IS");
+console.log(number2);
+console.log("RESULTS ARE:");
+console.log(number1 - number2);
+console.log("Multiplication of numbers to get number '8'");
+var number1 = 4;
+console.log("NUMBER 1 IS");
+console.log(number1);
+var number2 = 2;
+console.log("NUMBER 2 IS");
+console.log(number2);
+console.log("RESULTS ARE:");
+console.log(number1 * number2);
+console.log("Division of numbers to get number '8'");
+var number1 = 16;
+console.log("1 NUMBER IS");
+console.log(number1);
+var number2 = 2;
+console.log("NUMBER 2 IS");
+console.log(number2);
+console.log("RESULTS ARE:");
+console.log(number1 / number2);
